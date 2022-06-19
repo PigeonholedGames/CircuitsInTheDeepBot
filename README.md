70731746f46525566998fde11ee574ed9f69544a
PidgeonHoles <pidgeonholegames@gmail.com>
2022-06-19 13:36:06 +0300

    19/06 13:13 on join and >>setup command changes

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # CircuitsInTheDeepBot
 A Discord Bot for the Circuits In the Deep TTRPG
 
-It's designed to handle character creation, display and progression, dice rolling and other administrative tasks to make the game run smoother.
+It is designed to handle character creation, display and progression, dice rolling and other administrative tasks to make the game run smoother.
 
--- main.py
+++ main.py
@@ -3,65 +3,77 @@ from discord.utils import get
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
+
+# when the bot joins a server it creates a text channel category and text channels under it
 @client.event
 async def on_guild_join(server):
+    category = get(server.categories, name='Bot Controls')  # get category
 
-    category = get(server.categories, name='Bot Controls')
-
-    if category is None:
+    if category is None:  # check if category exists before making it
         category = await server.create_category_channel('Bot Controls')
 
-    if get(category.channels, name='character-creation') is None:
+    if get(category.channels, name='character-creation') is None:  # character-creation channel gets made
         await server.create_text_channel('character-creation', category=category)
 
-    if get(category.channels, name='rolls') is None:
+    if get(category.channels, name='rolls') is None:  # rolls channel gets made
         await server.create_text_channel('rolls', category=category)
 
-    if get(category.channels, name='clocks') is None:
+    if get(category.channels, name='clocks') is None:  # clocks channel gets made
         await server.create_text_channel('clocks', category=category)
 
+
+# listener for messages
 @client.event
 async def on_message(context):
+    # ignore the message if sent by the bot itself
     if context.author == client.user:
         return
 
+    # ignore all messages not starting with ">>"
     if context.content.startswith('>>'):
 
-        if context.content.startswith('>>create'):
-            print('create works')
-
-        if context.content.startswith('>>load'):
-            print('load works')
-
-        if context.content.startswith('>>show'):
-            print('show works')
-
+        # the roll command initiates a roll
         if context.content.startswith('>>roll'):
             print('roll works')
 
-        if context.content.startswith('>>clock'):
+        # the clock command creates a clock
+        elif context.content.startswith('>>clock'):
             print('clock works')
 
-        if context.content.startswith('>>setup'):
+        # the show command outputs the active characters sheet
+        elif context.content.startswith('>>show'):
+            print('show works')
+
+        # the create command creates a new thread and initializes a character creation sequence
+        elif context.content.startswith('>>create'):
+            print('create works')
+
+        # the load command changes the users active character
+        elif context.content.startswith('>>load'):
+            print('load works')
+
+        # the setup command remakes the category and channels that should have been created on join
+        elif context.content.startswith('>>setup'):
 
             server = context.guild
-            category = get(server.categories, name='Bot Controls')
+            category = get(server.categories, name='Bot Controls')  # get the category
 
-            if category is None:
+            if category is None:  # check if the category exists before making it
                 category = await server.create_category_channel('Bot Controls')
 
-            if get(category.channels, name='character-creation') is None:
+            if get(category.channels, name='character-creation') is None:  # character-creation channel gets made
                 await server.create_text_channel('character-creation', category=category)
 
-            if get(category.channels, name='rolls') is None:
+            if get(category.channels, name='rolls') is None:  # rolls channel gets made
                 await server.create_text_channel('rolls', category=category)
 
-            if get(category.channels, name='clocks') is None:
+            if get(category.channels, name='clocks') is None:  # clocks channel gets made
                 await server.create_text_channel('clocks', category=category)
 
 
-client.run(open('config.ini').readline())
+client.run(open('config.ini').readline())  # get the bot token from a local config file
